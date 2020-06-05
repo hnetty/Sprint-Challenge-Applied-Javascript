8cee93609426d375ffd40c1dd6c2065760271319
Harper Nettesheim <harpernettesheim@Harpers-MacBook-Pro.local>
2020-06-05 10:24:48 -0500

    Header is done

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,4 +11,28 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+
+    let mainDiv = document.createElement('div');
+    let dateSpan = document.createElement('span');
+    let heading = document.createElement('h1');
+    let tempSpan = document.createElement('span');
+    
+    mainDiv.appendChild(dateSpan);
+    mainDiv.appendChild(heading);
+    mainDiv.appendChild(tempSpan);
+
+    mainDiv.classList.add('header');
+    dateSpan.classList.add('date');
+    tempSpan.classList.add('temp');
+
+    dateSpan.textContent = 'MARCH 28, 2019';
+    heading.textContent = 'Lambda Times';
+    tempSpan.textContent = '98°';
+
+    return mainDiv;
+}
+
+let parentDiv = document.querySelector('.header-container');
+
+parentDiv.appendChild(Header());
