e33dcc95de77cd95c92fa2362a41805d7e93762e
Harper Nettesheim <harpernettesheim@Harpers-MacBook-Pro.local>
2020-06-05 15:35:11 -0500

    last changes

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,33 +18,56 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
-// axios.get('https://lambda-times-backend.herokuapp.com/articles')
-// .then(response => {
-//   console.log(response.data.articles);
-//   const items = Object.keys(response.data);
-//   console.log(items);
-//   debugger
-//   items.forEach(element => {
-//     createArticles(element);
-//   });
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+.then(response => {
+  console.log(response.data.articles);
+  const items1 = response.data.articles.javascript;
+  const items2 = response.data.articles.bootstrap;
+  const items3 = response.data.articles.technology;
+  const items4 = response.data.articles.jquery;
+  const items5 = response.data.articles.node;
+
+
+  console.log(items1, items2, items3, items4, items5);
+  debugger
   
-// })
-// .catch(error => {
-//   console.log(error)
-// },[])
+    items1.forEach(element => {
+        createArticles(element);
+    });
+    
+    items2.forEach(element => {
+        createArticles(element);
+    });
 
-const data = Object.keys({"articles":{"javascript":[{"headline":"ES8: The Next Step in the Evolution of Javascript and What it Means For Your Projects","authorPhoto":"./assets/sir.jpg","authorName":"SIR RUFF'N'STUFF"},{"headline":"Type Coercion: Why Does NaN !== NaN? and Other Strange Occurrences","authorPhoto":"./assets/bones.jpg","authorName":"BONES R. LIFE"},{"headline":"When to Rest, When to Spread: Why There Are Two Meanings Behind '...'","authorPhoto":"./assets/puppers.jpg","authorName":"PUPPER S. DOGGO"},{"headline":"Typescript: Ten Things you Should Know Before Building Your Next Angular Application","authorPhoto":"./assets/sir.jpg","authorName":"SIR RUFF'N'STUFF"}],"bootstrap":[{"headline":"Bootstrap 5: Get a Sneak Peak at all the New Features","authorPhoto":"./assets/fido.jpg","authorName":"FIDO WALKSALOT"},{"headline":"UI Frameworks: A Comparison, Which Made Our List? (Hint: Bootstrap is on it)","authorPhoto":"./assets/max.jpg","authorName":"MAX GOODBOYE"},{"headline":"The Hottest New Bootstrap 4 Components Every Developer Needs to Know About","authorPhoto":"./assets/max.jpg","authorName":"MAX GOODBOYE"}],"technology":[{"headline":"AI: What Are the Ethical Ramifications of the Future?","authorPhoto":"./assets/max.jpg","authorName":"MAX GOODBOYE"},{"headline":"AR or VR: How We Will See the World Through a Digital Lense","authorPhoto":"./assets/max.jpg","authorName":"MAX GOODBOYE"},{"headline":"Quantum Computing: Where we Are Now, and Where We Will Be Very Soon","authorPhoto":"./assets/max.jpg","authorName":"MAX GOODBOYE"}],"jquery":[{"headline":"History of Javascript Libraries: from jQuery to React","authorPhoto":"./assets/puppers.jpg","authorName":"PUPPER S. DOGGO"},{"headline":"jQuery Animations: When and Where to Use Them, and Not CSS","authorPhoto":"./assets/fido.jpg","authorName":"FIDO WALKSALOT"},{"headline":"Why I Use jQuery in Every Application I Create and Why You Should Too","authorPhoto":"./assets/fido.jpg","authorName":"FIDO WALKSALOT"}],"node":[{"headline":"Node.js in 2018: Which Framework? Express, Sails, Hapi?","authorPhoto":"./assets/sir.jpg","authorName":"SIR RUFF'N'STUF"},{"headline":"MongoDB: NoSQL vs. SQL, the Debate Continues..","authorPhoto":"./assets/bones.jpg","authorName":"BONES R. LIFE"}]}});
+    items3.forEach(element => {
+        createArticles(element);
+    });
 
-data.forEach(element => {
-    createArticles(element);
-});
+    items4.forEach(element => {
+        createArticles(element);
+    });
+
+    items5.forEach(element => {
+        createArticles(element);
+    });
+
+
+})
+.catch(error => {
+  console.log(error)
+},[])
+
+
+
+
 
 
 function createArticles(information){
-    console.log(Object.keys(information));
+
+    
     
     let parentDiv = document.querySelector('.tabs');
-    debugger
+    
 
     for (let i = 0; i < information.length; i++){
         debugger
@@ -61,9 +84,9 @@ function createArticles(information){
 function newArticle(position){
 
     let sectionDiv = document.createElement('div');
-
-    for (let i = 0; i < 4; i++){
-        debugger;
+    debugger;
+    for (let i = 0; i < position.length; i++){
+        
         
         let articleCard = document.createElement('div');
         let articleHeadline = document.createElement('div');
@@ -87,3 +110,7 @@ function newArticle(position){
     return sectionDiv;
 
 }
+
+
+
+// const data = Object.keys({"articles":{"javascript":[{"headline":"ES8: The Next Step in the Evolution of Javascript and What it Means For Your Projects","authorPhoto":"./assets/sir.jpg","authorName":"SIR RUFF'N'STUFF"},{"headline":"Type Coercion: Why Does NaN !== NaN? and Other Strange Occurrences","authorPhoto":"./assets/bones.jpg","authorName":"BONES R. LIFE"},{"headline":"When to Rest, When to Spread: Why There Are Two Meanings Behind '...'","authorPhoto":"./assets/puppers.jpg","authorName":"PUPPER S. DOGGO"},{"headline":"Typescript: Ten Things you Should Know Before Building Your Next Angular Application","authorPhoto":"./assets/sir.jpg","authorName":"SIR RUFF'N'STUFF"}],"bootstrap":[{"headline":"Bootstrap 5: Get a Sneak Peak at all the New Features","authorPhoto":"./assets/fido.jpg","authorName":"FIDO WALKSALOT"},{"headline":"UI Frameworks: A Comparison, Which Made Our List? (Hint: Bootstrap is on it)","authorPhoto":"./assets/max.jpg","authorName":"MAX GOODBOYE"},{"headline":"The Hottest New Bootstrap 4 Components Every Developer Needs to Know About","authorPhoto":"./assets/max.jpg","authorName":"MAX GOODBOYE"}],"technology":[{"headline":"AI: What Are the Ethical Ramifications of the Future?","authorPhoto":"./assets/max.jpg","authorName":"MAX GOODBOYE"},{"headline":"AR or VR: How We Will See the World Through a Digital Lense","authorPhoto":"./assets/max.jpg","authorName":"MAX GOODBOYE"},{"headline":"Quantum Computing: Where we Are Now, and Where We Will Be Very Soon","authorPhoto":"./assets/max.jpg","authorName":"MAX GOODBOYE"}],"jquery":[{"headline":"History of Javascript Libraries: from jQuery to React","authorPhoto":"./assets/puppers.jpg","authorName":"PUPPER S. DOGGO"},{"headline":"jQuery Animations: When and Where to Use Them, and Not CSS","authorPhoto":"./assets/fido.jpg","authorName":"FIDO WALKSALOT"},{"headline":"Why I Use jQuery in Every Application I Create and Why You Should Too","authorPhoto":"./assets/fido.jpg","authorName":"FIDO WALKSALOT"}],"node":[{"headline":"Node.js in 2018: Which Framework? Express, Sails, Hapi?","authorPhoto":"./assets/sir.jpg","authorName":"SIR RUFF'N'STUF"},{"headline":"MongoDB: NoSQL vs. SQL, the Debate Continues..","authorPhoto":"./assets/bones.jpg","authorName":"BONES R. LIFE"}]}});
